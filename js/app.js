657a9517349cb853a244ee1d4ea86f04b12ece4b
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2018-06-30 13:19:31 -0400

    Add Files, Initial Commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB0gc0qfZpFuBBSKr9CW9re15NEJp9885Q",
+    authDomain: "master-d3v.firebaseapp.com",
+    databaseURL: "https://master-d3v.firebaseio.com",
+    projectId: "master-d3v",
+    storageBucket: "master-d3v.appspot.com",
+    messagingSenderId: "1072322384561"
+};
+firebase.initializeApp(config);
